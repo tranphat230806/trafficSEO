15b900e5079da8c81f007ca099c5e03c2d83bc17
tranphat230806 <phat.tran09192005@gmail.com>
2026-08-26 10:43:52 +0700

    update api for page

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // Configure backend URL (can be changed based on environment)
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = 'https://trafficseo.onrender.com/api';
 
 /**
  * Helper to show/hide loading overlay
